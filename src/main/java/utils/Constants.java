7d95334594cae82215def00598c49c5a007da5ba
Junxi-HM <em.chen.junxi@gmail.com>
2025-04-29 11:55:46 +0200

    change #2 replace hardcoded with constants

---
+++ src/main/java/utils/Constants.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package utils;
+
+/**
+ *
+ * @author emche
+ */
+public class Constants {
+    public static final String STORAGE_ARRAYLIST = "ArrayList";
+    public static final String STORAGE_HASHMAP = "HashMap";
+    public static final String STORAGE_FILE = "File";
+    public static final String STORAGE_FILE_SERIALIZATION = "File (Serialization)";
+    public static final String STORAGE_SQL = "SQL - Database";
+    public static final String STORAGE_JPA = "JPA - Database";
+}
